fae90c16f404d0068597e1996e97374e13727618
anodim <sauvanetthierry@gmail.com>
2022-07-25 21:01:28 +0200

    init

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        
+        Console.WriteLine("answer");
+    }
+}
